193ed0ffde0946806c6413bb6c7add8f5bb50bfb
arsummers <aliyasummers1@gmail.com>
2019-07-09 22:14:07 -0700

    able to take multiple numbers as input

--- game_of_greed.py
+++ game_of_greed.py
@@ -3,6 +3,7 @@ import random
 def determine_score(vals):
     # between 1 and 6 values
     # test saved dice for scoring
+
     return None
 
 total_score = 0
@@ -12,6 +13,16 @@ current_round = 1
 active_dice = []
 saved_dice = []
 
+def start_game():
+    start_game_prompt = input('Are you ready to start a game of greed?')
+    
+    if start_game_prompt == 'Y':
+        print('Here are your starting dice: ')
+        # dice roll function is called after this in the call stack below
+
+    elif start_game_prompt == 'N':
+        print('you have exited the game')
+
 
 def roll_dice(dice):
     for i in range(6):             
@@ -20,78 +31,97 @@ def roll_dice(dice):
     print(active_dice)
     return dice
 
-def current_active_dice():
-    # global active_dice
-    initial_dice = roll_dice(active_dice)
+def set_aside_dice():
+    print('Which dice do you wish to set aside any of these dice for scoring?')
+    saved_dice = [int(n) for n in input().split(' ')]
 
-    # This just keeps the list of active dice from getting too long, and resets it for future rolls
-    if len(active_dice) > 6:
-        # active_dice = []
-        next_dice = roll_dice(active_dice)
-        print(f'This is your next round: {next_dice}')
+    for i in saved_dice:
+        active_dice.remove(i)
+    print(f'These are the dice you saved: {saved_dice}')
+    print(f'These are the dice you have remaining: {active_dice}')
 
-    # These are the dice I want to work with
-    if len(initial_dice) <= 6:
-        print(active_dice)
+
+# def current_active_dice():
+#     # global active_dice
+#     initial_dice = roll_dice(active_dice)
+
+#     # This just keeps the list of active dice from getting too long, and resets it for future rolls
+#     if len(active_dice) > 6:
+#         # active_dice = []
+#         next_dice = roll_dice(active_dice)
+#         print(f'This is your next round: {next_dice}')
+
+#     # These are the dice I want to work with
+#     if len(initial_dice) <= 6:
+#         print(active_dice)
     
 
-def save_from_roll():
-    save_from_roll_prompt = input('Which dice to you wish to save for scoring?')
+# def save_from_roll():
+#     save_from_roll_prompt = int(input('Which dice to you wish to save for scoring?'))
 
-    saved_dice_values = str(save_from_roll_prompt)
-    print(f'These are the values of the saved dice: {saved_dice_values}')
+#     saved_dice_values = save_from_roll_prompt
+#     print(f'These are the values of the saved dice: {saved_dice_values}')
 
-    # is this a subsection of the dice pool - going to need something other than in
-    if saved_dice_values in active_dice:
-        saved_dice.append(save_from_roll_prompt)
-        active_dice.remove(save_from_roll_prompt)
-        print(f'Here are the dice you banked: {saved_dice}')
-        print(f'Here are your remaining dice: {active_dice}')
+#     # is this a subsection of the dice pool - going to need something other than in
+#     if saved_dice_values in active_dice:
+#         saved_dice.append(save_from_roll_prompt)
+#         active_dice.remove(save_from_roll_prompt)
+#         print(f'Here are the dice you banked: {saved_dice}')
+#         print(f'Here are your remaining dice: {active_dice}')
 
-    elif saved_dice_values not in active_dice:
-        print('That was not a valid die. Don\'t be a cheater!')
+#     elif saved_dice_values not in active_dice:
+#         print('That was not a valid die. Don\'t be a cheater!')
 
-# TODO: find a good way to repeat this
-def save_these_dice_flow():
-    global current_round
+# # TODO: find a good way to repeat this
+# def save_these_dice_flow():
+#     global current_round
 
+#     while len(saved_dice) < 6:
+#         save_decision_prompt = input('Do you want to save any of these dice?')
 
-    while len(saved_dice) <= 6:
-        save_decision_prompt = input('Do you want to save any of these dice?')
-
-        if save_decision_prompt == 'Y':
-            current_round += 1
-            save_from_roll()
-        if save_decision_prompt == 'N':
-            current_round += 1
-            print(f'The dice will now reroll. Start of round {current_round}')
-        # TODO: work on this bit - it isn't re-rendering as needed
-            save_from_roll()
-            save_decision_prompt = input('Do you want to save any of these dice?')
-            print(active_dice)
-            # current_active_dice()
-        
-            continue
-        # not quite what I'm after but close
-        # save_these_dice_flow()
+#         if save_decision_prompt == 'Y':
+#             current_round += 1
+#             save_from_roll()
+#         if save_decision_prompt == 'N':
+#             current_round += 1
+#             print(f'The dice will now reroll. Start of round {current_round}')
+#         # TODO: work on this bit - it isn't re-rendering as needed
+#             save_from_roll()
+#             save_decision_prompt = input('Do you want to save any of these dice?')
+#             print('These are from line 70', active_dice)
+#             # current_active_dice()
+#             roll_dice(active_dice)       
+#             continue
 
 
-def start_game():
-    start_game_prompt = input('Are you ready to start a game of greed?')
-    print('Here are your starting dice: ')
-    
 
-    if start_game_prompt == 'Y':
-        # current_active_dice()
-        # roll_dice is working better
-        roll_dice(active_dice)
-        # save_these_dice_flow()
+def play_round():
+    roll_dice(active_dice)
+    set_aside_dice()
 
-    elif start_game_prompt == 'N':
-        print('you have exited the game')
+    # let user pick dice
+        # let user pick multiple dice at once
+    # when user is done picking dice:
+        # calculate score for the round
+        # give option to:
+            # re-roll remaining dice
+                # if re-rolled and no score, zero out score for the round
+            # bank score and start a new round with 6 dice
+                # add banked score to total score
+
+
+
 # should be in name == main thing
+
 start_game()   
-    
+play_round()
+
+
+# roll_dice(active_dice)  
+# save_these_dice_flow()
+# determine_score()
+
+# pick dice to keep 
 
 # TODO: select multiple dice at once. Successfully remove from active pool. Write tests for scoring
 
--- test_game_of_greed.py
+++ test_game_of_greed.py
@@ -1,8 +1,18 @@
 # __if name__ == "__main__"
 
 from game_of_greed import determine_score
+from game_of_greed import roll_dice
 
 def test_1():
     # expected
     # actual
     assert determine_score([1]) == 100
+
+
+
+def test_roll_dice():
+    # not sure how to set this up
+    # I need it to check that the length of the active dice list <= 6 and that none of their values are higher than 6
+    expected = ['this is here for me to fill in later']
+    actual = ['so is this']
+    assert expected == actual
